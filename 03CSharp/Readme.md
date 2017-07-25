05ff64354c23661b4d90a58ab025b8e573ec1a57
Doug Seelinger <doseelin@microsoft.com>
2017-07-25 13:29:05 -0400

    Added C# Hello

---
+++ 03CSharp/Readme.md
@@ -0,0 +1,7 @@
+# Install
+
+All Platforms - [https://www.microsoft.com/net/core](https://www.microsoft.com/net/core)
+
+# Build & Run
+* dotnet restore
+* dotnet run
---
+++ 03CSharp/_hello/HelloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Hello 
+{
+    public static class HelloWorld
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
