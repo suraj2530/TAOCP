ee6e14eabcac7ecc987fef3c836bac81686a9490
Doug Seelinger <doseelin@microsoft.com>
2017-07-26 19:52:58 -0400

    Java example 1.1

---
+++ 01Java/ex1_1/Main.java
@@ -0,0 +1,17 @@
+class Main {
+    public static void main(String argv[]) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+
+        int t = a;
+
+        a = b;
+        b = c;
+        c = d;
+        d = t;
+
+        System.out.format("%d, %d, %d, %d", a, b, c, d);
+    }
+}
