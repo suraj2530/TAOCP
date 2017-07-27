ee858f20865593aeda8661c84fff4e8fe71f72ae
Doug Seelinger <doseelin@microsoft.com>
2017-07-26 22:04:00 -0400

    Added C# ex 1.1

---
+++ 03C#/ex1_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            int c = 3;
+            int d = 4;
+
+            int t = a;
+
+            a = b;
+            b = c;
+            c = d;
+            d = t;
+
+            Console.WriteLine($"{a}, {b}, {c}, {d}");
+        }
+    }
+}
